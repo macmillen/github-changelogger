b066be8d12fcb5143e26ace707b8df63f1ec3047
Milan Jaritz <milan.jaritz@gmail.com>
2021-10-19 12:38:43 +0200

    init

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  type Entry = { url: string };
+
+  let values: Entry[] = [];
+</script>
+
+{#each values as url, i}
+  <input type="text" bind:value={values[i].url} />
+{/each}
+
+<button on:click={() => (values = [...values, { url: "" }])}>Add</button>
