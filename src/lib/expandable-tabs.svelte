b91a54ba8180f7654df0716d24979e0638086839
Milan Jaritz <milan.jaritz@gmail.com>
2021-11-02 11:22:48 +0100

    add commits tab

---
+++ src/lib/expandable-tabs.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import TabButton from "./tab-button.svelte";
+  import UpdatesNotifier from "./updates-notifier.svelte";
+
+  type ExpansionType = "changelog" | "commits";
+
+  export let onExpand: ((type: ExpansionType) => void) | undefined = undefined;
+  export let changelogUpdates: boolean;
+
+  let expanded: ExpansionType | null = null;
+
+  const onClick = (type: ExpansionType) => {
+    if (expanded !== type) {
+      expanded = type;
+      onExpand?.(type);
+    } else expanded = null;
+  };
+</script>
+
+<div class="flex gap-1">
+  <TabButton
+    selected={expanded === "changelog"}
+    on:click={() => onClick("changelog")}
+  >
+    {#if changelogUpdates}
+      <UpdatesNotifier />
+    {/if}
+    CHANGELOG
+  </TabButton>
+  <TabButton
+    selected={expanded === "commits"}
+    on:click={() => onClick("commits")}
+  >
+    COMMITS
+  </TabButton>
+</div>
+<div>
+  {#if expanded === "changelog"}
+    <slot name="changelog" />
+  {:else if expanded === "commits"}
+    <slot name="commits" />
+  {/if}
+</div>
---
+++ src/lib/loader.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import Spinner from "./spinner.svelte";
+</script>
+
+<div class="text-white flex items-center gap-3 animate-pulse">
+  <span> LOADING </span>
+  <Spinner />
+</div>
