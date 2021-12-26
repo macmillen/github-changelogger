f49f17fa50d402bd941f1349ca66291505b75e27
macmillen <milan.jaritz@gmail.com>
2021-12-26 15:05:29 +0100

    add cool icons

--- src/lib/delete-button.svelte
+++ src/lib/delete-button.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import IconDelete from "./icon-delete.svelte";
+  import Icon from "@iconify/svelte";
 </script>
 
 <button class="px-2 text-red-600 hover:text-red-300 transition" on:click>
-  <IconDelete />
+  <Icon icon="ph:trash-bold" class="w-5 h-5" />
 </button>
