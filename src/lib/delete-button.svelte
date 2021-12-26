a8c8dd4c5cedf3a850901388b12e108cd4ddb598
Milan Jaritz <milan.jaritz@gmail.com>
2021-12-26 14:13:54 +0100

    another restyling of delete button

--- src/lib/delete-button.svelte
+++ src/lib/delete-button.svelte
@@ -2,9 +2,6 @@
   import IconDelete from "./icon-delete.svelte";
 </script>
 
-<button
-  class="px-2 bg-red-600/50 hover:bg-red-800 transition ring-4 ring-red-900/50 text-white rounded-md"
-  on:click
->
+<button class="px-2 text-red-600 hover:text-red-300 transition" on:click>
   <IconDelete />
 </button>
